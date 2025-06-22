ac1e086a9234dc2deba3eaee08169ac96e15fa8d
Simonhajd <92337591+Simonhajd@users.noreply.github.com>
2025-06-22 12:41:38 -0400

    Delete journalrefresher.py

--- journalrefresher.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-
-with open("journal.md", "a") as file:
-    file.write("\n#"+datetime.now().strftime("%B %d: ") + "\n")
-    file.write("\n** Total time spent: **\n")
